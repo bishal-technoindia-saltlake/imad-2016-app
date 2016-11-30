9624704c734ef8e957278ecb23c6edd18ebc3aa4
bishal-technoindia-saltlake <bishalfirstahuman@gmail.com>
2016-11-30 16:13:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/article-one', function (req, res) {
   res.send("Article two requested and will be served here");
 });
  
- app.get('/article-two', function (req, res) {
-  res.send("Article two requested and will be served here");
+ app.get('/article-three', function (req, res) {
+  res.send("Article three requested and will be served here");
 });
  
  
