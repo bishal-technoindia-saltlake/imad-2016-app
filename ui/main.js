bc785a0653fb36698abf1f808b5a747aa1cd0338
bishal-technoindia-saltlake <bishalfirstahuman@gmail.com>
2016-12-04 17:00:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML='A new Value';
 var img=document.getElementById('pic');
 var marginLeft=0;
 function moveRight(){
-   marginLeft=marginLeft+'5';
+   marginLeft=marginLeft+'1';
    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick = function() {
