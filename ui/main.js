fc22b4c04cba494c1341545b6c49a616dfdcf580
bishal-technoindia-saltlake <bishalfirstahuman@gmail.com>
2016-12-04 16:28:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 //Change the text of main text
 var element = document.getElementById('main-text');
 element.innerHTML='A new Value';
+var img=document.getElementById('pic');
+img.onclick = function() {
+img.style.marginLeft='100px';
+};
